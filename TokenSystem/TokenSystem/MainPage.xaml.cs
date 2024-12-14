e7984de1de7cacef56df8e2df497123be8d20b9e
RaziyaShajeer <rasiyaaitrich.com>
2024-12-14 11:53:09 +0530

    message

---
+++ TokenSystem/TokenSystem/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TokenSystem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TokenSystem/TokenSystem/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TokenSystem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
